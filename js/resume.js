6c488df23dc25f4dd8355d13f1729f3f1ab2dcc0
Alain Nicolas <alain.nicolas@talan.com>
2020-11-27 23:29:19 +0100

    Mise à jour du Fri Nov 27 23:29:19 CET 2020

--- js/resume.js
+++ js/resume.js
@@ -27,4 +27,18 @@
         });
     });
 
+    let k = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65],
+        n = 0;
+    $(document).keydown(function (e) {
+        if (e.keyCode === k[n++]) {
+            if (n === k.length) {
+                $('.img-profile').attr('src', "img/baby-yoda.webp");
+                n = 0;
+                return false;
+            }
+        } else {
+            n = 0;
+        }
+    });
+
 })(jQuery); // End of use strict
