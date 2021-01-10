330aabdc52a598c6e50ab0044950e68f182c1aa1
alainncls <alain.nicolas.perso@gmail.com>
2021-01-10 23:20:19 +0100

    Mise à jour du Sun Jan 10 23:20:18 CET 2021

--- js/resume.js
+++ js/resume.js
@@ -4,7 +4,7 @@
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
+            let target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
                 $('html, body').animate({
@@ -17,6 +17,7 @@
 
     // Closes responsive menu when a scroll trigger link is clicked
     $('.js-scroll-trigger').click(function () {
+        console.log('CLOSE')
         $('.navbar-collapse').collapse('hide');
     });
 
@@ -27,16 +28,16 @@
         });
     });
 
-    let k = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 38, 38, 40, 40, 37, 39, 37, 39, 66, 65],
-        n = 0;
+    const konami = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+    let n = 0;
     $(document).keydown(function (e) {
-        if (e.keyCode === k[n++]) {
-            if (n === k.length / 2) {
+        if (e.keyCode === konami[n++]) {
+            if (n === konami.length / 2) {
                 const profilPicture = $('.img-profile');
                 profilPicture.attr('src', profilPicture.attr('src').slice(0, profilPicture.attr('src').lastIndexOf('/')) + '/baby-yoda.webp');
                 return false;
             }
-            if (n === k.length) {
+            if (n === konami.length) {
                 const profilPicture = $('.img-profile');
                 profilPicture.attr('src', profilPicture.attr('src').slice(0, profilPicture.attr('src').lastIndexOf('/')) + '/penguin-chick.gif');
                 n = 0;
@@ -44,6 +45,32 @@
             }
         } else {
             n = 0;
+        }
+    });
+
+    const dark = [68, 65, 82, 75];
+    let o = 0;
+    $(document).keydown(function (e) {
+        if (e.keyCode === dark[o++]) {
+            if (o === dark.length) {
+                const styleSheet = document.createElement('style');
+                styleSheet.textContent = '  html {\n' +
+                    '    filter: invert(1) hue-rotate(.5turn);\n' +
+                    '  }\n' +
+                    '\n' +
+                    '  img {\n' +
+                    '    filter: invert(1) hue-rotate(.5turn);\n' +
+                    '  }\n' +
+                    '\n' +
+                    '  img:not(:hover) {\n' +
+                    '    opacity: .7;\n' +
+                    '    transition: opacity .25s ease-in-out;\n' +
+                    '  }';
+                document.body.appendChild(styleSheet);
+                return false;
+            }
+        } else {
+            o = 0;
         }
     });
 
