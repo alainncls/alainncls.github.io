4b6e73bd3eb9e8a9c67aed2d9ca9a92cba8e69d2
Alain Nicolas <alain.nicolas@consensys.net>
2026-05-08 19:17:48 +0200

    fix: deploy versioned service worker

---
+++ js/service-worker-registration.min.02973c7f293c5a09c7ee28dcd94f48d5951f0e7ffb02d6e9288b35988a1f539b.js
@@ -0,0 +1,1 @@
+"serviceWorker"in navigator&&window.addEventListener("load",()=>{navigator.serviceWorker.register("/service-worker.js?v=portfolio-assets-v1",{scope:"/",updateViaCache:"none"}).catch(()=>{})})
